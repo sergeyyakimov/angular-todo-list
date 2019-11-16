f1d262d2d68bb0751dc2fabd653cc44f1fad53d1
SergeyYakimov <teriertis@yandex.ru>
2019-11-16 21:04:23 +0800

    Made todo-list application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'ng-minin-youtube';
+  appTitle = 'Angular todo application'
 }
